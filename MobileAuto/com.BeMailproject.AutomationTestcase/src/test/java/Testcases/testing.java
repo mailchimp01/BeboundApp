013850af4708ae224456598c7edf43c01e377188
DinhN <dinh.java1111@gmail.com>
2016-10-25 18:37:06 +0800

    update for generating report

---
+++ MobileAuto/com.BeMailproject.AutomationTestcase/src/test/java/Testcases/testing.java
@@ -0,0 +1,26 @@
+package Testcases;
+
+import org.testng.annotations.Test;
+
+public class testing {
+	@Test
+	public void testcase1(){
+		System.out.println("a");
+	}
+	
+	@Test
+	public void testcaseb(){
+		System.out.println("b");
+	}
+	
+	@Test
+	public void testcasec(){
+		System.out.println("d");
+	}
+	
+	@Test
+	public void testcased(){
+		System.out.println("d");
+	}
+
+}
